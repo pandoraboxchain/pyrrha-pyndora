9e83f5d1f46aaca1f2c54900d24a665b824e545b
Andrey Sobol <sobol@pandora.foundation>
2017-10-03 03:29:58 +0300

    main worker

--- worker.py
+++ worker.py
@@ -1,8 +1,14 @@
 from states.idle import Idle
+from eth.connector import ETHConnector
+from ipfs.connector import IPFSConnector
+from config.config import CONFIGS
 
 class Worker():
 
     def __init__(self):
+        self.eth = ETHConnector(CONFIGS["ETH"])
+        self.eth.run()
+        self.ipfs = IPFSConnector(CONFIGS["IPFS"])
         self.state = Idle()
 
 if __name__ == '__main__':
