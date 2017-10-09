5dc92da950eaffa8805662d30bbe3684bda61fdc
Andrey Sobol <sobol@pandora.foundation>
2017-10-09 16:55:17 +0200

    assigned status

---
+++ states/assigned.py
@@ -0,0 +1,17 @@
+from states.base import State
+from states.validating_data import ValidatingData
+from states.events.cognitive_job_created import CognitiveJobCreated
+from states.events.validation_started import ValidationStarted
+
+class Assigned(State):
+
+    def on_event(self, event):
+        if type(event) == CognitiveJobCreated:
+            contract = event.job_contract
+            count = contract.call().activeWorkersCount()
+            for item in range(count):
+                worker = contract.call().activeWorkers(item)
+                if worker == self.worker.eth.config['worker']:
+                    self.change_state(ValidatingData())
+                    self.worker.state.on_event(ValidationStarted(contract, event.job_contract_address))
+        return self
